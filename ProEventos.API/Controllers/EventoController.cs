1835549e1dcde05773b3627098b7763f588f35af
ibsenquaresma <39648650+ibsenquaresma@users.noreply.github.com>
2022-12-22 13:21:52 -0500

    Section_3_Introdução_ao_EF_Core

---
+++ ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EventoController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public EventoController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IEnumerable<Evento> GetAll()
+    {
+        return _context.Eventos;
+    }
+
+    [HttpGet("{id}")]
+    public IEnumerable<Evento> GetById(int id)
+    {
+        return _context.Eventos.Where(x=> x.EventoId == id);
+    }
+
+    [HttpPost]
+    public string Post()
+    {
+        return "Post";
+    }
+
+    [HttpPut("{id}")]
+    public string Put(int id)
+    {
+        return "Put " + id;
+    }
+
+    [HttpDelete("{id}")]
+    public string Delete(int id)
+    {
+        return "Delete " + id;
+    }
+}
