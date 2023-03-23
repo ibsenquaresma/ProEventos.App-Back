29a441c9791d5a733031308fa9e090686732e1cd
ibsenquaresma <39648650+ibsenquaresma@users.noreply.github.com>
2023-03-23 11:24:12 -0400

    Section_13_Asp.NET_Core_Identity_Auth_JWT_Back

--- ProEventos.Application/Dtos/UserUpdateDto.cs
+++ ProEventos.Application/Dtos/UserUpdateDto.cs
@@ -8,16 +8,16 @@ namespace ProEventos.Application.Dtos
     public class UserUpdateDto
     {
         public int Id { get; set; }
-        public string Titulo { get; set; }
-        public string UserName { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Funcao { get; set; }
-        public string Descricao { get; set; }
-        public string Password { get; set; }
-        public string Token { get; set; }
-        public string ImagemURL { get; set; }
+        public string? Titulo { get; set; }
+        public string? UserName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Funcao { get; set; }
+        public string? Description { get; set; }
+        public string? Password { get; set; }
+        public string? Token { get; set; }
+        public string? ImagemURL { get; set; }
     }
 }
